e6fe533e176c6899d914bcc62d2a926e6e93d080
Jason Zaffuto <jason@artificesecurity.com>
2024-04-10 02:52:30 -0600

    Initial commit of Metasploitable3-Fixed

---
+++ scripts/installs/install_openjdk6.bat
@@ -0,0 +1,2 @@
+powershell -Command "[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://bitbucket.org/alexkasko/openjdk-unofficial-builds/downloads/openjdk-1.7.0-u80-unofficial-windows-amd64-installer.zip', 'C:\Windows\Temp\openjdk-1.6.0-unofficial-b27-windows-amd64.zip')" <NUL
+cmd /c ""C:\Program Files\7-Zip\7z.exe" x "C:\Windows\Temp\openjdk-1.6.0-unofficial-b27-windows-amd64.zip" -oC:\openjdk6"
